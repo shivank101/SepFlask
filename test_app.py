dff219ecf08ef4a51a9af109ffef0fd179992cfe
shivank101 <shivank101@gmail.com>
2024-10-07 08:34:58 +0530

    save

--- test_app.py
+++ test_app.py
@@ -10,4 +10,10 @@ def test_home(client):
     resp = client.get('/ping')
     assert resp.status_code == 200
 
+def test_predict(client):
+    test_data={'Gender':"Male", 'Married':"Unmarried",'Credit_History' : "Unclear Debts",'ApplicantIncome':100000,'LoanAmount':2000000}
+    resp=client.post('/prediction', json=test_data)
+    assert resp.status_code == 404
+    assert resp.json=={'loan_approval_status': 'Rejected'}
+
 
